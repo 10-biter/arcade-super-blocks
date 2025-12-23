86c2f1292ae7cfad588a14b78b38b5bbf63324d2
10-biter <ep.student@hotmail.com>
2025-12-23 15:28:33 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,11 @@
 //% icon="\uf192" color=#005953
 namespace more {
 
-    /**
-     * Move sprite up by some pixels
-     */
-    //% block="move $sprite up by $pixels"
-    //% pixels.defl=5
-    export function moveUp(sprite: Sprite, pixels: number) {
-        sprite.y -= pixels
-    }
-
-    /**
-     * Move sprite down by some pixels
-     */
-    //% block="move $sprite down by $pixels"
-    //% pixels.defl=5
-    export function moveDown(sprite: Sprite, pixels: number) {
-        sprite.y += pixels
-    }
 
 
-    //% block="move the $thing with speed $speed"
-    export function move(thing:Sprite, speed?:number): void {
+    //% block="move the $thing to the $side with speed $speed"
+    //% thing.defl="mySprite"
+    export function move(thing:Sprite, side:Sides, speed:number): void {
         
     }
     
