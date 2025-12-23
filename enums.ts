069bbda53a397507456b2de5838ee7587aded773
10-biter <ep.student@hotmail.com>
2025-12-23 15:24:07 +0300

    Update pxt.json, main.ts, enums.ts

---
+++ enums.ts
@@ -0,0 +1,10 @@
+enum Sides {
+    //% block="up"
+    Up,
+    //% block="down"
+    Down,
+    //% block="right"
+    Right,
+    //% block="left"
+    Left
+}
--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
 //% icon="\uf192" color=#005953
-namespace simple {
+namespace more {
+
+    /**
+     * Move sprite up by some pixels
+     */
+    //% block="move $sprite up by $pixels"
+    //% pixels.defl=5
+    export function moveUp(sprite: Sprite, pixels: number) {
+        sprite.y -= pixels
+    }
+
+    /**
+     * Move sprite down by some pixels
+     */
+    //% block="move $sprite down by $pixels"
+    //% pixels.defl=5
+    export function moveDown(sprite: Sprite, pixels: number) {
+        sprite.y += pixels
+    }
 
 
-
-    //% block
-    export function foo(): void {
+    //% block="move the $thing with speed $speed"
+    export function move(thing:Sprite, speed?:number): void {
         
     }
+    
 }
